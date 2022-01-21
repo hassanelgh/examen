0f7027cf92b2d1c5b3d65be00239e346c5e4d2b1
hassanelgh <elghalbzourihassan1@gmail.com>
2022-01-21 12:28:52 +0100

    v1

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Movies");
+  let moviesF = fs.readFileSync('movies_reduit.json');
+  let movies = JSON.parse(moviesF);
+
+  res.json(movies)
 });
 
 module.exports = router;
